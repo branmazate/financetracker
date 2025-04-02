f4adb81e38c2db9c39c57b9c3442aeb156c614e0
Fermazate <fernandomazate@gmail.com>
2025-04-02 15:19:41 -0600

    ADD: Dashboard Screen composable function with a column with current balance and a PieChart for expenses grouped by category

---
+++ app/src/main/java/com/example/financetracker/ui/screens/dashboard/DashboardScreen.kt
@@ -0,0 +1,22 @@
+package com.example.financetracker.ui.screens.dashboard
+
+import android.content.Context
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import com.example.financetracker.ui.viewmodels.DashboardViewModel
+import com.github.mikephil.charting.charts.PieChart
+
+@Composable
+fun DashboardScreen (viewModel:DashboardViewModel){
+    val expenses by viewModel.expensesByCategory.collectAsState(emptyList())
+    val balance by viewModel.currentBalance.collectAsState(0.0)
+
+    Column {
+        Text("Current Balance: $ $balance", style = MaterialTheme.typography.titleSmall)
+        PieChart(expenses as Context?)
+    }
+}
